c05e7cafd816e4a135a3562e2758aeec36cd86bf
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-25 04:48:09 +0530

    rolex: Happy New Year 2018

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Copyright (C) 2017 - The LineageOS Project
+Copyright (C) 2018 - The LineageOS Project
 
 Device configuration for Xiaomi Redmi 4A ("rolex")
 ==================================================
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ set -e
 DEVICE=rolex
 VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2017
+INITIAL_COPYRIGHT_YEAR=2016
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"

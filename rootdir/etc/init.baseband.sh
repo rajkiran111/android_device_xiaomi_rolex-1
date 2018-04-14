c103443beff66e1477f4714acb5597107ea8167c
ECr34T1v3 <eriikgarciia1999@gmail.com>
2018-04-14 13:59:41 +0000

    Revert "Update baseband script added tail for fix baseband unknown"
    This reverts commit b15cdfa0257a81f6c6304bb30bbe6a7c3168d465.

--- rootdir/etc/init.baseband.sh
+++ rootdir/etc/init.baseband.sh
@@ -27,4 +27,4 @@
 #
 
 # grep the modem partition for baseband version and set it
-setprop gsm.version.baseband `strings /dev/block/bootdevice/by-name/modem | grep "^MPSS.JO." | tail -1`
+setprop gsm.version.baseband `strings /dev/block/bootdevice/by-name/modem | grep "^MPSS.JO." | head -1`

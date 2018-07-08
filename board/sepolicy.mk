ca5eea41914fe5a88f27cbd4cf158e6cc00e37d2
ECr34T1v3 <eriikgarciia1999@gmail.com>
2018-07-08 17:30:32 +0000

    rolex/riva: SEPolicy and their problems

--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,10 +1,5 @@
 # SELinux
-<<<<<<< HEAD
-BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy
-=======
 #BOARD_SEPOLICY_DIRS += \
 #    $(VENDOR_PATH)/sepolicy
->>>>>>> parent of 05faa91... msm8917-common: Clean and enable sepolicy
 
 #include device/qcom/sepolicy/sepolicy.mk

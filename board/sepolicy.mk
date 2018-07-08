965f907b3ca5e609358229f84ed7d4457096c83c
ECr34T1v3 <eriikgarciia1999@gmail.com>
2018-07-08 17:26:03 +0000

    Revert "msm8917-common: Clean and enable sepolicy"
    This reverts commit 05faa9103c7edae0809544b64ab7faae614f24c8.

--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,5 +1,10 @@
 # SELinux
+<<<<<<< HEAD
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
+=======
+#BOARD_SEPOLICY_DIRS += \
+#    $(VENDOR_PATH)/sepolicy
+>>>>>>> parent of 05faa91... msm8917-common: Clean and enable sepolicy
 
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk

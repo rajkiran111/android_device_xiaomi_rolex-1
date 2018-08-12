ceecc133b9f65025eb6fdd7cefac8523695588e0
LuK1337 <priv.luk@gmail.com>
2018-08-12 16:10:01 +0200

    [DNM] rolex: Treble sucks
    Change-Id: I304cbfbfba8c7a4c403a8e865bf0d940cac1bea2

--- product/gps.mk
+++ product/gps.mk
@@ -18,3 +18,6 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so

355e6947e0977b92ece9715d6ff03cb3b287286a
ECr34T1v3 <eriikgarciia1999@gmail.com>
2018-08-27 13:52:35 +0000

    rolex/riva: AEX Pie Bringup

--- lineage_rolex.mk
+++ lineage_rolex.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 AospExtended Project
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,12 +15,15 @@
 # limitations under the License.
 #
 
+#Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Aex stuff
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
@@ -29,7 +32,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := aosp_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
